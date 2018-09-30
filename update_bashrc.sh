8b1204f1aade7c1d173c3bf5083caefeab4b18c8
Nikola Tasic <ntasic7@gmail.com>
2018-09-30 03:35:59 +0200

    compatability

--- update_bashrc.sh
+++ update_bashrc.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 loc="source \$HOME/Documents/CODE/sh/bashrc/.bashrc"
-if grep $loc ~/.bashrc
+if grep "$loc" "$HOME/.bashrc"
 then
     exec bash
 else 
-    echo "$loc" >> ~/.bashrc
+    echo "$loc" >> "$HOME/.bashrc"
     exec bash
 fi
